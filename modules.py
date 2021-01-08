480610991f344619b1ab8cec30e500b56e4305a4
Penta0308 <38977908+Penta0308@users.noreply.github.com>
2021-01-08 15:15:25 +0900

    Update modules.py

--- modules.py
+++ modules.py
@@ -42,7 +42,7 @@ async def update_loop(bot: commands.Bot):
 
 
 def update_task(bot: commands.Bot):
-    asyncio.run(update_loop(bot))
+    asyncio.new_event_loop().run_until_complete(update_loop(bot))
 
 
 async def get_popular_article(bot: commands.Bot):
